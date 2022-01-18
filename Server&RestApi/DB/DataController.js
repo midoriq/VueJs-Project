508dd3ed531fc84b7d78cc0091438d37a1775a4e
poneciak57 <poneciak@gmail.com>
2022-01-18 12:47:58 +0100

    More accurate measurments throughout the day

--- Server&RestApi/DB/DataController.js
+++ Server&RestApi/DB/DataController.js
@@ -7,7 +7,7 @@ function createData(Data,Tname){
 function getAllData(Tname){
     return knex(Tname).select("*");
 };
-async function getData(Tname,nrRows){
+async function getWeather(nrRows){
     const maxId = await knex(Tname).select("id");
     return knex(Tname).select("*").where("id",'>',maxId.length - nrRows);
 }
@@ -17,18 +17,31 @@ function getLongitudeLatitude(name){
 function selectSpecific(Tname,Rname,Content){
     return knex(Tname).select(Rname).where(Rname,'=',Content);
 };
+async function updateWeatherMeasurment(Data){
+    const maxId = await knex("Weather").select("id");
+    const lastMeasurment = await knex("Weather").select("*").where("id",'=',maxId.length).then(result => result[0]);
+    const Mnr = lastMeasurment.nrMeasurments;
 
-// function deleteData(id,Tname){
-//     return knex(Tname).where("id",id).del();
-// };
-// function updateData(id,Data,Tname){
-//     return knex(Tname).where("id",id).update(Data);
-// };
+    const newMeasurment = '{}';
+    const obj = JSON.parse(newMeasurment);
+    obj.Id = lastMeasurment.Id;
+    obj.Temp = Math.round((parseFloat(Data.Temp) + parseFloat(lastMeasurment.Temp*Mnr)) / (Mnr+1) *100)/100;
+    obj.Feels_like = Math.round((parseFloat(Data.Feels_like) + parseFloat(lastMeasurment.Feels_like*Mnr))/(Mnr+1) *100)/100;
+    obj.MinTemp = Math.round((parseFloat(Data.MinTemp) + parseFloat(lastMeasurment.MinTemp*Mnr)) / (Mnr+1) *100)/100;
+    obj.MaxTemp = Math.round((parseFloat(Data.MaxTemp) + parseFloat(lastMeasurment.MaxTemp*Mnr)) / (Mnr+1) *100)/100;
+    obj.Pressure = Math.round((parseFloat(Data.Pressure) + parseFloat(lastMeasurment.Pressure*Mnr)) / (Mnr+1) *100)/100;
+    obj.Humidity = Math.round((parseFloat(Data.Humidity) + parseFloat(lastMeasurment.Humidity*Mnr)) / (Mnr+1) *100)/100;
+    obj.nrMeasurments = (Mnr+1);
+
+    console.log(obj);
+    return knex("Weather").where("id",'=',maxId.length).update(obj);
+};
 
 module.exports = {
     createData,
     getAllData,
-    getData,
+    getWeather,
     getLongitudeLatitude,
-    selectSpecific
+    selectSpecific,
+    updateWeatherMeasurment
 }
